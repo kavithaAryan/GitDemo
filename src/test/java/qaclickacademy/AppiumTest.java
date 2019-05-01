8600bfdafb96dc27dba716d97b5abcbbafc92c0b
Kavitha <KavithaAryan@gmail.com>
2019-05-01 11:06:48 +0530

    This is my first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeApp()
+	{
+		System.out.println("nativeApp");
+	}
+	@Test
+	public void iosApp()
+	{
+		System.out.println("iosApp");
+	}
+
+}
